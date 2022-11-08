00730c8df77a39e3bfbfdb8ea1182240d4ebc8d5
margfiraa <104053257+margfiraa@users.noreply.github.com>
2022-11-08 23:10:44 +0700

    Add files via upload

---
+++ Pemesan.java
@@ -0,0 +1,16 @@
+public class Pemesan{
+    private String nama;
+    
+    public Pemesan()
+    {
+        
+    }
+    
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+    
+    public String getNama(){
+        return this.nama;
+    }
+}
