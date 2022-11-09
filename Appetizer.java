59c22a8328f0fa1528b46ebf68f34781127e42fa
stnrsita <101649065+stnrsita@users.noreply.github.com>
2022-11-09 21:19:59 +0700

    Update Appetizer.java
    add javadoc at class Appetizer

--- Appetizer.java
+++ Appetizer.java
@@ -1,11 +1,18 @@
 public class Appetizer extends Makanan
 {
-
+    /**
+     * Appetizer 
+     * @param n merupakan variable yang ada pada super class Makanan
+     * @param h juga merupakan variabel yang ada pada super class Makanan
+     */
     public Appetizer(String n, double h) 
     {
         super(n, h);
     }
 
+    /**
+     * display1 berfungsi untuk mengoverride abstract method dari class Makanan
+     */
     @Override
     public void display1() 
     {
@@ -13,6 +20,9 @@ public class Appetizer extends Makanan
         System.out.println("--------------------------------------");       
     }
 
+    /**
+     * display berfungsi untuk menampilkan nama dan harga makanan dari class Appetizer
+     */
     @Override
     public void display()
     {
