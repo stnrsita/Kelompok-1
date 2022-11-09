a0cb66d2ba791859ebc0942fa06d5cd0229abf86
Dhaifina Alifa Putri <100226971+nayloe28@users.noreply.github.com>
2022-11-09 07:24:38 +0700

    perbaiki class dessert
    pengaplikasian inheritance dan polimorfisme

--- Dessert.java
+++ Dessert.java
@@ -1,51 +1,17 @@
-public class Dessert extends Makanan
-{
-    String nama;
-    double harga;
+public class Dessert extends Makanan{
 
-    public Dessert() 
-    {
-        
-    }
-    
-    public Dessert(String n , double h)
-    {
-        this.nama = n;
-        this.harga = h;
+    public Dessert(String n, double h) {
+        super(n, h);
     }
 
     @Override
-    public String getNama() 
-    {
-        return nama;
-    }
-
-    public void setNama(String nama) 
-    {
-        this.nama = nama;
-    }
-    
-    public double getHarga() 
-    {
-        return harga;
-    }
-
-    @Override
-    public void setHarga(double harga) 
-    {
-        this.harga = harga;
-    }
-    
-    @Override
-    public void display1()
-    {
+    public void display1() {
         System.out.println("\tSpecial Desserts\t");
-        System.out.println("--------------------------------------");    
+        System.out.println("--------------------------------------");       
     }
 
-    @Override
-    public void display()
-    {
-        System.out.println(this.nama+" : "+ "$" + this.harga);
+    public void display(){
+        System.out.println(super.nama+" : "+ "$" + super.harga);
     }
+    
 }
