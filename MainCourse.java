78a46347e6302e74d120a46ad9a15c3ff98de1fc
margfiraa <104053257+margfiraa@users.noreply.github.com>
2022-11-09 21:39:06 +0700

    Update MainCourse.java

--- MainCourse.java
+++ MainCourse.java
@@ -1,43 +1,42 @@
-public class MainCourse extends Makanan{
-    String nama;
-    double harga;
-
-    public MainCourse() {
-        
+/**
+ * class MainCourse dibuat dengan menggunakan konsep abstract class dan inheritance.
+ * Dalam konsep inheritance, class ini berperan sebagai subclass.
+ * Class ini berisi method yang mengoverride method abstarct yang ada di class Makanan (konsep abstract).
+ * Class ini juga mewarisi method tidak abstract yang ada di class Makanan (konsep inheritance).
+ * 
+ * @author (1. Dhaifina Alifa Putri-2108107010018)
+ * @author (2. Margfirah-2108107010021)
+ * @author (3. Siti Nurrahmasita-2108107010015)
+ * @version (08-10-2022)
+ */
+public class MainCourse extends Makanan
+{
+    /**
+     * MainCourse
+     * @param n merupakan variable yang ada pada super class Makanan.
+     * @param h juga merupakan variabel yang ada pada super class Makanan.
+     */
+    public MainCourse(String n, double h) 
+    {
+        super(n, h);
     }
-    
-    public MainCourse(String n , double h){
-        this.nama = n;
-        this.harga = h;
 
-    }
-
+    /**
+     * display1 berfungsi untuk mengoverride abstract method dari class Makanan.
+     */
     @Override
-    public String getNama() {
-        return nama;
+    public void display1() 
+    {
+        System.out.println("\tSpecial Main Course\t");
+        System.out.println("--------------------------------------");       
     }
 
-    public void setNama(String nama) {
-        this.nama = nama;
-    }
-    
-    public double getHarga() {
-        return harga;
-    }
-
+    /**
+     *display berfungsi untuk menampilkan nama dan harga makanan dari class MainCourse.
+     */
     @Override
-    public void setHarga(double harga) {
-        this.harga = harga;
-    }
-    
-    @Override
-    public void display1(){
-        System.out.println("\tSpecial MainCourses\t");
-        System.out.println("--------------------------------------");
-    }
-
-    @Override
-    public void display(){
-        System.out.println(this.nama+" : "+ "$" + this.harga);
+    public void display()
+    {
+        System.out.println(super.nama+" : "+ "$" + super.harga);
     }
 }
