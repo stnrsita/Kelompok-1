f446e02d689e8a0aa02284583c9ee36654bcb9ae
stnrsita <101649065+stnrsita@users.noreply.github.com>
2022-11-09 21:42:47 +0700

    Update Appetizer.java
    update javadoc at class Appetizer

--- Appetizer.java
+++ Appetizer.java
@@ -1,3 +1,14 @@
+/**
+ * class Appetizer dibuat dengan menggunakan konsep abstract class dan inheritance.
+ * Dalam konsep inheritance, class ini berperan sebagai subclass.
+ * Class ini berisi method yang mengoverride method abstarct yang ada di class Makanan (konsep abstract).
+ * Class ini juga mewarisi method tidak abstract yang ada di class Makanan (konsep inheritance).
+ * 
+ * @author (1. Dhaifina Alifa Putri-2108107010018)
+ * @author (2. Margfirah-2108107010021)
+ * @author (3. Siti Nurrahmasita-2108107010015)
+ * @version (08-10-2022)
+ */
 public class Appetizer extends Makanan
 {
     /**
