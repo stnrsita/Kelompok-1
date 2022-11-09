e505051429d600ea835e05e22fadbaccca5c0254
stnrsita <101649065+stnrsita@users.noreply.github.com>
2022-11-09 09:08:26 +0700

    Update Appetizer.java

--- Appetizer.java
+++ Appetizer.java
@@ -1,52 +1,21 @@
 public class Appetizer extends Makanan
 {
-    String nama;
-    double harga;
 
-    public Appetizer() 
+    public Appetizer(String n, double h) 
     {
-        
-    }
-    
-    public Appetizer(String n , double h)
-    {
-        this.nama = n;
-        this.harga = h;
-
+        super(n, h);
     }
 
     @Override
-    public String getNama() 
-    {
-        return nama;
-    }
-
-    public void setNama(String nama) 
-    {
-        this.nama = nama;
-    }
-    
-    public double getHarga() 
-    {
-        return harga;
-    }
-
-    @Override
-    public void setHarga(double harga) 
-    {
-        this.harga = harga;
-    }
-    
-    @Override
-    public void display1()
+    public void display1() 
     {
         System.out.println("\tSpecial Appetizers\t");
-        System.out.println("--------------------------------------");    
+        System.out.println("--------------------------------------");       
     }
 
     @Override
     public void display()
     {
-        System.out.println(this.nama+" : "+ "$" + this.harga);
+        System.out.println(super.nama+" : "+ "$" + super.harga);
     }
 }
