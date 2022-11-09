65934bb5b99821812896734b8643b355dff17355
stnrsita <101649065+stnrsita@users.noreply.github.com>
2022-11-09 21:20:59 +0700

    Update Pemesan.java
    add javadoc at class Pemesan

--- Pemesan.java
+++ Pemesan.java
@@ -1,16 +1,107 @@
-public class Pemesan{
+public class Pemesan
+{
+    //instance variables
     private String nama;
-    
+    private double input;
+    private double total;
+    private int jumlah;
+    private double harga;
+
+    /**
+     * Constructor for objects of class hh
+     */
     public Pemesan()
     {
         
     }
     
-    public void setNama(String nama){
+    /**
+     * setNama untuk mengatur nama pelanggan
+     * @param nama untuk mengubah nama pelanggan
+     */
+    public void setNama(String nama)
+    {
         this.nama = nama;
     }
     
-    public String getNama(){
+    /**
+     * getNama untuk mendapatkan nama pelanggan
+     * @return this.nama untuk mengembalikan informasi dasar dari nama pelanggan
+     */
+    public String getNama()
+    {
         return this.nama;
+    }
+
+    /**
+     * setInput untuk mengatur inputan saat memilih menu dan jumlah
+     * @param i sebagai inisialisasi inputan untuk mengubah inputan
+     */
+    public void setInput(double i)
+    {
+        this.input = i;
+    }
+
+    /**
+     * getInput untuk mendapatkan inputan pelanggan
+     * @return this.input untuk mengembalikan informasi dasar dari inputan pelanggan
+     */
+    public double getInput()
+    {
+        return this.input;
+    }
+
+    /**
+     * getHarga untuk mendapatkan nilai harga
+     * @return this.harga untuk mengembalikan informasi dasar harga makanan
+     */
+    public double getHarga() 
+    {
+        return this.harga;
+    }
+
+    /**
+     * setHarga untuk menetapkan nilai harga
+     * @param harga untuk mengubah harga makanan sesuai dengan jumlah yang diinput
+     */
+    public void setHarga(double harga)
+    {
+        this.harga = harga;
+    }
+
+    /**
+     * setJumlah untuk menetapkan nilai jumlah
+     * @param j sebagai inisialisasi untuk mengubah jumlah pesanan
+     */
+    public void setJumlah(int j)
+    {
+        this.jumlah = j;
+    }
+
+    /**
+     * getHarga untuk mendapatkan nilai jumlah
+     * @return this.harga untuk mengembalikan informasi dasar harga makanan
+     */
+    public int getJumlah()
+    {
+        return this.jumlah;
+    }
+
+    /**
+     * setTotal untuk menetapkan nilai total
+     * @param total untuk mengubah total pesanan
+     */
+    public void setTotal(double total)
+    {
+        this.total = total;
+    }
+    
+    /**
+     * getHarga untuk mendapatkan nilai total harga
+     * @return this.total untuk mengembalikan informasi dasar total harga pesanan
+     */
+    public double getTotal()
+    {
+        return this.total;
     }
 }
