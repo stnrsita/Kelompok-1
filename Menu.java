5510e5eb8ad63a0662ea62545646b2f1c419e08f
Dhaifina Alifa Putri <100226971+nayloe28@users.noreply.github.com>
2022-11-09 07:44:24 +0700

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -14,31 +14,30 @@ public class Menu{
         s.close();
         System.out.println();
         p.setNama(namaPemesan);
-               
-        Appetizer a1 = new Appetizer("Bruschetta",25.00);
-        Appetizer a2 = new Appetizer("Fried Calamari", 18.00);
-        Appetizer a3 = new Appetizer("Shrimp Scampi", 20.00);
-        List<Appetizer> AppetizerList = new ArrayList<>();
-
-        AppetizerList.add(0,a1 );
-        AppetizerList.add(1,a2);
-        AppetizerList.add(2,a3);
 
         System.out.println("+-------------------------------------+");
         System.out.println("|        Today's Special Menu         |");
         System.out.println("+-------------------------------------+");
         System.out.println();
-    
+               
+        Makanan a1 = new Appetizer("Bruschetta",25.00);
+        Makanan a2 = new Appetizer("Fried Calamari", 18.00);
+        Makanan a3 = new Appetizer("Shrimp Scampi", 20.00);
+        List<Makanan> AppetizerList = new ArrayList<>();
+
+        AppetizerList.add(0,a1 );
+        AppetizerList.add(1,a2);
+        AppetizerList.add(2,a3);
+
         a1.display1();
-        for(Appetizer Appetizer : AppetizerList){
+        for(Makanan Appetizer : AppetizerList){
             Appetizer.display();
         }
     
-        MainCourse mc1 = new MainCourse("Butter Chicken", 40.00);
-        MainCourse mc2 = new MainCourse("Beef Barley Soup", 45.00);
-        MainCourse mc3 = new MainCourse("Shrimp Pesto Pasta", 43.00);
-
-        List<MainCourse> MainCourseList = new ArrayList<>();
+        Makanan mc1 = new MainCourse("Butter Chicken", 40.00);
+        Makanan mc2 = new MainCourse("Beef Barley Soup", 45.00);
+        Makanan mc3 = new MainCourse("Shrimp Pesto Pasta", 43.00);
+        List<Makanan> MainCourseList = new ArrayList<>();
 
         MainCourseList.add(0,mc1);
         MainCourseList.add(1,mc2);
@@ -46,15 +45,14 @@ public class Menu{
 
         System.out.println();
         mc1.display1();
-        for(MainCourse MainCourse : MainCourseList){
+        for(Makanan MainCourse : MainCourseList){
             MainCourse.display();
         }
 
-        Dessert d1 = new Dessert("Costro Tiramisu", 15.00);
-        Dessert d2 = new Dessert("Chocolate Maple", 10.00);
-        Dessert d3 = new Dessert("Apple Pie", 10.00);
-
-        List<Dessert> DessertList = new ArrayList<>();
+        Makanan d1 = new Dessert("Costro Tiramisu", 15.00);
+        Makanan d2 = new Dessert("Chocolate Maple", 10.00);
+        Makanan d3 = new Dessert("Apple Pie", 10.00);
+        List<Makanan> DessertList = new ArrayList<>();
 
         DessertList.add(0,d1);
         DessertList.add(1,d2);
@@ -62,7 +60,7 @@ public class Menu{
 
         System.out.println();
         d1.display1();
-        for(Dessert Dessert : DessertList){
+        for(Makanan Dessert : DessertList){
             Dessert.display();
         }
         System.out.println();
